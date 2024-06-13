62eb85a0df6148714f0b7aa9df460e5d790221a0
Jon-Ethan Richards <45516368+JE-Richards@users.noreply.github.com>
2024-06-13 13:18:50 +0100

    Add GameController factory and bug fixes
    The GameController contains the logic to play the game and is playable
    within the browser console. The function also returns two additional
    functions, getCurrentPlayer and getGameStatus, which aren't needed to
    play the game in the console but will be useful for the browser UI.
    
    Bug Fixes:
    - The playerInput() function inside the Gameboard factory didn't work.
    The if conditions inside the function weren't looking up the value of
    the selected cell, and so the code was never being executed. Fixed this
    by correctly referencing the cell values within the if condition.
    - Fixed the cell function to add a player token. The code was written
    before building the player objects. As such, it was written to update
    the cell values to be the player object rather than the players token.
    Fixed this by extracting the token from the player object.
    
    Additional Notes:
    - Pseudocode has been left in the script for now to help explain the
    thought process behind the code. When the script is finalised, the
    pseudocode can be removed.

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function Gameboard() {
         // function to override value
         const addToken = (player) => {
             // value = player token
-            value = player;
+            value = player.token;
         }
 
         // function to get cell value
@@ -39,10 +39,10 @@ function Gameboard() {
     const playerInput = (row, column, player) => {
         // check if cell is empty (value = 0);
         // if not empty move invalid so end loop
-        if (board[row][column] !== 0) {
+        if (board[row][column].getValue() !== 0) {
             return;
         }
-        else if (board[row][column] === 0)
+        else if (board[row][column].getValue() === 0)
             board[row][column].addToken(player);
     }
 
@@ -57,4 +57,196 @@ function Gameboard() {
 
     // return get board, user input, and print board
     return { getBoard, playerInput, printBoard };
+}
+
+// create game controller factory
+// Accepts two inputs, a name for player 1 and a name for player 2
+// Default names provided if no inputs given
+function GameController(playerOneName = "Player One", playerTwoName = "Player Two") {
+    // create an array containing player information
+    // needs the name and the corresponding token
+    // for now offer a default token of x and o
+    const players = [
+        { name: playerOneName, token: 1 },
+        { name: playerTwoName, token: 2 }
+    ]
+
+    // Instantiate the gameboard
+    const board = Gameboard();
+
+    // Pick current player
+    // For now, default to player 1
+    let currentPlayer = players[0];
+
+    // Logic to switch from one player to another
+    const switchPlayer = () => {
+        // if current player = 1, set to 2
+        // if current player = 2, set to 1
+        // use ternary over longer if statement
+        currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
+    }
+
+    // function to return the current player
+    const getCurrentPlayer = () => currentPlayer;
+
+    // set a game status
+    // 0 in progress
+    // 1 player one wins
+    // 2 player two wins
+    // 3 tie game
+    let gameStatus = 0;
+
+    // function to get the game status after updated value
+    const getGameStatus = () => gameStatus;
+
+    // Win / lose / tie logic
+    const winConditions = () => {
+        // if no combinations possible -> tie game
+            // if all cells.getValue() !== 0 then no moves left
+        if (board.getBoard().every(row => row.every(cell => cell.getValue() !== 0))) {
+            gameStatus = 3;
+            getGameStatus();
+            return;
+        }
+
+        // Check for all combinations of 3 in a row
+        // 3 in a row for rows
+        for (let row = 0; row < 3; row ++) {
+            if (
+                (board.getBoard()[row][0].getValue() === board.getBoard()[row][1].getValue()) &&
+                (board.getBoard()[row][0].getValue() === board.getBoard()[row][2].getValue())
+            ) {
+                // switch to avoid needing to use nested ifs
+                switch (board.getBoard()[row][0].getValue()) {
+                    case 0:
+                        break;
+                    
+                    case 1:
+                        gameStatus = 1;
+                        getGameStatus();
+                        break;
+                    
+                    case 2:
+                        gameStatus = 2;
+                        getGameStatus();
+                        break;
+                }
+            }
+        }
+
+        // 3 in a row for columns
+        for (let col = 0; col < 3; col++) {
+            if (
+                (board.getBoard()[0][col].getValue() === board.getBoard()[1][col].getValue()) &&
+                (board.getBoard()[0][col].getValue() === board.getBoard()[2][col].getValue())
+            ) {
+                switch (board.getBoard()[0][col].getValue()) {
+                    case 0:
+                        break;
+                    
+                    case 1:
+                        gameStatus = 1;
+                        getGameStatus();
+                        break;
+                    
+                    case 2:
+                        gameStatus = 2;
+                        getGameStatus();
+                        break;
+                }
+            }
+        }
+
+        // 3 in a row for diagonals
+        // leading diagonal
+        if (
+            (board.getBoard()[0][0].getValue() === board.getBoard()[1][1].getValue()) &&
+            (board.getBoard()[0][0].getValue() === board.getBoard()[2][2].getValue())
+        ) {
+            switch (board.getBoard()[0][0].getValue()) {
+                case 0:
+                    break;
+                
+                case 1:
+                    gameStatus = 1;
+                    getGameStatus();
+                    break;
+                
+                case 2:
+                    gameStatus = 2;
+                    getGameStatus();
+                    break;
+            }
+        }
+    
+        // reverse diagonal
+        if (
+            (board.getBoard()[0][2].getValue() === board.getBoard()[1][1].getValue()) &&
+            (board.getBoard()[0][2].getValue() === board.getBoard()[2][0].getValue())
+        ) {
+            switch (board.getBoard()[0][2].getValue()) {
+                case 0:
+                    break;
+                
+                case 1:
+                    gameStatus = 1;
+                    getGameStatus();
+                    break;
+                
+                case 2:
+                    gameStatus = 2;
+                    getGameStatus();
+                    break;
+            }
+        }
+    }
+
+    // function to display a new round
+    // includes logic to declare win, loss, tie
+    const displayRound = () => {
+        // print the board
+        board.printBoard();
+        // print the player who's turn it is, or if the game has finished
+        switch (gameStatus) {
+            case 0:
+                console.log(`It is currently ${currentPlayer.name}'s turn.`)
+                break;
+
+            case 1:
+                console.log(`GAME OVER!!!\n${players[0].name} WINS!!!`)
+                return;
+            
+            case 2:
+                console.log(`GAME OVER!!!\n${players[1].name} WINS!!!`)
+                return;
+
+            case 3:
+                console.log(`GAME OVER!!!\nIT'S A TIE, NO MOVES REMAINING!`)
+        }
+    }
+
+    // function to play a round
+    const playRound = (row, col) => {
+        // Accept user input to make the move - row, column input into the play round function
+        // need to make sure that if the cell is already populated with a player token, then playerInput is called again
+        board.playerInput(row, col, currentPlayer)
+        // log the move they take
+        console.log(`${currentPlayer.name} chose to put their token in row ${row}, column ${col}`)
+        // run switch player function
+        switchPlayer();
+        getCurrentPlayer();
+        // check win / lose / tie
+            // if win / lose / tie end game
+        winConditions();
+        // log new round
+        displayRound();
+    }
+
+    // call the function to display the round to start the game
+    displayRound();
+
+    // return function to play round
+    // return get current player for UI use later
+    // return get game status to program logic in the UI to stop the game from being playable when the game is over
+    return { playRound, getCurrentPlayer, getGameStatus }
 }
